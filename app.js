452a5af4713fadcd4a200819dd27710906dae99a
Mirko <ruhlmirkojoel@gmail.com>
2025-04-03 09:46:25 -0300

    Finished version of the scroll animation

--- app.js
+++ app.js
@@ -3,12 +3,18 @@
 const marqueElement = document.querySelector("#marquee .wrapper");
 const langList = ["Javascript", "Python", "Html", "CSS", "Json"];
 
+const numItems = 5;
+const totalDuration = 30; // in seconds
+const styleSheet = document.styleSheets[0];
+
 for (let i in langList) {
   const newItem = document.createElement("div");
   newItem.classList.add(`item`, `item-${i}`);
   newItem.textContent = langList[i];
 
   marqueElement.appendChild(newItem);
-}
 
-//<div class="item"></div>
+  const delay = (totalDuration / numItems) * (numItems - (Number(i) + 1)) * -1;
+  const rule = `.item-${i} { animation-delay: ${delay}s; }`;
+  styleSheet.insertRule(rule, styleSheet.cssRules.length);
+}
