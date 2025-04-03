8a05ed054208a0c91d30345c1b8dfba0ad1b3941
Mirko <ruhlmirkojoel@gmail.com>
2025-04-03 10:05:26 -0300

    Updated responsive marquee design

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 "use strict";
 
 const marqueElement = document.querySelector("#marquee .wrapper");
-const langList = ["Javascript", "Python", "Html", "CSS", "Json"];
+const langList = [
+  "Javascript",
+  "Python",
+  "Html",
+  "CSS",
+  "Json",
+  "bootstrap5",
+  "Tailwind",
+  "tkinter",
+  "ttkbootstrap",
+];
 
-const numItems = 5;
+const numItems = langList.length;
 const totalDuration = 30; // in seconds
 const styleSheet = document.styleSheets[0];
 
 for (let i in langList) {
   const newItem = document.createElement("div");
   newItem.classList.add(`item`, `item-${i}`);
-  newItem.textContent = langList[i];
+  newItem.innerHTML = `<span>${langList[i]}</span>`;
 
   marqueElement.appendChild(newItem);
 
