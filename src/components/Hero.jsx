79cf705466936dec32cd532c453556ea78ac6390
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-11-05 17:37:04 -0300

    Add shortcut links with icons to Hero section
    Introduced a set of shortcut links with Devicon icons in the Hero component for quick access. Updated styles to support the new shortcut links, including icon styling and improved link/button appearance. Also added the Devicon stylesheet to index.html and made minor structural improvements in App.jsx.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
 export default function Hero() {
   return <section id="hero">
-      <h1>Mirko Ruhl</h1> 
+      <h1>Mirko Joel Ruhl</h1> 
       <p>I'm a full stack programmer that loves graphic design.</p>
-      <div className="shortcut-links">
-        <a href="#about" className="button"></a>
-      </div>
+      <ul className="shortcut-links">
+        <a href="#about" className="button">                   
+          <i className="devicon-linkedin-plain shortcut-icon"></i>          
+        </a>
+        <a href="#about" className="button">                   
+          <i className="devicon-github-original shortcut-icon"></i>          
+        </a>
+        <a href="#about" className="button">                   
+          <i className="devicon-linkedin-plain shortcut-icon"></i>          
+        </a>
+      </ul>
     </section>;
 }
