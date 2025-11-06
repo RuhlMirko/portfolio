bd882d0a69245c301b7a855618afeb83d9940379
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-11-05 23:48:08 -0300

    Added second project item

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,30 @@
 import tenziesImg from '../assets/tenzies.png';
+import assemBlyImg from '../assets/assembly.png';
 
 export default function Projects() {
   return (
     <section id="projects">
       <h2>My Projects</h2>
-      <p>Here are some of the projects I have worked on.</p>
       <ul>
         <li >
           <a href="" className='project-card'>
             <div className='project-info'>
               <h3>Tenzies game</h3>
-              <p>This project</p>
+              <p>This project mimics the game of tenzies where you have to have all of the same face of dice to win the game</p>
             </div>
             
             <img src={tenziesImg} alt="Tenzies game project" />
-          </a>
-          
+          </a>          
+        </li>
+
+        <li>
+          <a href="" className='project-card'>
+            <div className='project-info'>
+              <h3>Assembly: Endgame</h3>
+              <p>This game is basically HangMan but with a programming twist to it</p>
+            </div>            
+            <img src={assemBlyImg} alt="" />
+          </a>        
         </li>
       </ul>
     </section>
