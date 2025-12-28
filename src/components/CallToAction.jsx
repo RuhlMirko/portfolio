49cc292642a98119b0b39e9022fac859c0da57a5
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-28 17:34:12 -0300

    Added call to action section

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,14 +1,14 @@
 export default function CallToAction() {
   return (
-    <section id='cta'>   
+    <section className='cta'>   
         <div className="cta-info">
-            <h2>Let's Work Together</h2>
+            <h2 className="hover-slide">Let's Work Together</h2>
             <p>Have a project in mind? I'm available for freelance work and collaborations.</p>
         </div> 
         
         <div className="cta-links">
-            <a href="" className="cta-btn">Linkedin</a>            
-            <a href="" className="cta-btn">Email</a>
+            <button>LinkedIn</button>
+            <button>Email</button>
         </div>
         
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ export default function Header() {
   return (
     <nav>      
       <ul>
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About Me</a></li>
+        <li><a href="#hero">Home</a></li>        
         <li><a href="#projects">Projects</a></li>
         <li><a href="#skills">Skills</a></li>
+        <li><a href="#about">About Me</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
