eee85cf09f3c41a3b405af71c9a13c073a5bfe09
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-29 01:05:21 -0300

    Added a database with my projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,19 @@
-import tenziesImg from '../assets/tenzies.png';
-import assemBlyImg from '../assets/assembly.png';
-import chefClaudeImg from '../assets/chefClaude.png';
+import { ul } from 'motion/react-client';
 import TechUsed from './TechUsed';
 import React from 'react';
 
 export default function Projects() {
+  const projects = ['tenzies','vanlife','assembly']
   const [currProj, setCurrProj] = React.useState(null)
+
+
+  // const ProjectMenu = projects.map((item)=>{
+  //   return (
+  //     <ul>
+  //       <li className='hover-slide'><button>Tenzies</button></li>
+  //     </ul>
+  //   )
+  // })
 
   return (
     <section id="projects">
@@ -18,11 +26,13 @@ export default function Projects() {
             <li className='hover-slide'><button>Van Life <span>React Router</span></button></li>
             <li className='hover-slide'><button>Tenzies game</button></li>
           </ul>
+          {/* <ProjectMenu /> */}
 
           <div className='project-card'>
             {/* <h3>projectsData.tenzies.title</h3> */}
-            <h3>Tenzies Game</h3>
+            <h3 className='project-title'>Tenzies Game</h3>
             <p>{/* projectsData.tenzies.description*/}</p>
+            {/* <TechUsed tech='html5'/> */}
           </div>
 
         </div>
