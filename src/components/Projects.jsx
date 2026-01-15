64b3b35a2418f64b02b5c4cf35e2b9e59c1824b0
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-01-15 01:12:17 -0300

    Added labels to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,19 +35,22 @@ export default function Projects() {
     <section id="projects">
       <h2 className='hover-slide section-title' >My Projects</h2>
       <div className='projects-container'>          
-          <ul className='projects-menu'>
-            <li className='hover-slide'><button onClick={()=>toggleProj(0)} className={currProj === 0 ? 'active-tab ':''}>Tenzies game <span>React</span></button></li>
-            <li className='hover-slide'><button onClick={()=>toggleProj(1)} className={currProj === 1 ? 'active-tab ':''}>Chef Claude <span>React</span></button></li>
-            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 2 ? 'active-tab ':''}>Van Life <span>React Router</span></button></li>            
-            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 3 ? 'active-tab ':''}>Components Library <span>React</span></button></li>            
-            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 4 ? 'active-tab ':''}>Assembly Endgame<span>React</span></button></li>            
-            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 5 ? 'active-tab ':''}>Basketball Timer<span>React Router</span></button></li>            
-            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 6 ? 'active-tab ':''}>Van Life <span>React Router</span></button></li>            
+          <ul className='projects-menu'>            
+            <li className='project-menu-lbl'>React</li>
+            <li className='hover-slide'><button onClick={()=>toggleProj(0)} className={currProj === 0 ? 'active-tab ':''}>Tenzies game </button></li>
+            <li className='hover-slide'><button onClick={()=>toggleProj(1)} className={currProj === 1 ? 'active-tab ':''}>Chef Claude </button></li>
+            <li className='project-menu-lbl'>React Router</li>
+            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 2 ? 'active-tab ':''}>Van Life </button></li>            
+            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 3 ? 'active-tab ':''}>Components Library </button></li>            
+            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 4 ? 'active-tab ':''}>Assembly Endgame</button></li>            
+            <li className='project-menu-lbl'>JavaScript</li>
+            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 5 ? 'active-tab ':''}>Basketball Timer</button></li>            
+            <li className='hover-slide'><button onClick={()=>toggleProj(2)} className={currProj === 6 ? 'active-tab ':''}>Van Life </button></li>            
           </ul>
-          <div className='project-cards-wrapper'>
-            {projectsElements}
-          </div>
+        <div className='project-cards-wrapper'>
+          {projectsElements}
         </div>
+      </div>
     </section>
   );
 }
